7b0c938d0cab5e88f734c1fc36f7f3bb48f36c43
MRATTE <matthias.ratte@gmail.com>
2025-02-28 16:11:46 +0100

    Update Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -1,16 +1,18 @@
 
-def readTemp(I2C):
-    buf = bytearray(1)  # création d’un buffer 1 octet
-    buf[0] = 0x00  # adresse du registre point
-    I2C.writeto(0x48, buf)  # écriture dans le LM75
-    mesure = I2C.readfrom(0x48, 2)  # lecture 
-    print("temperature en bin:", bin(mesure[0]), bin(mesure[1]))
-    val = (mesure[0] << 3)|(mesure[1] >> 5) #reduction de 16 à 11bits.
-    print(val)
-    if (val > 1023):
-        res =  (val ^ 0b11111111111)+1
-        temperature = - res * 0.125
-    else :
-        temperature = val * 0.125
-    print("temperature:",temperature)
-    return temperature
+class lecture_Temperature:
+    def __init__(self, i2c):
+          self.i2c = i2c
+
+    def readTemp(self):
+        buf = bytearray(1)  # création d’un buffer 1 octet
+        buf[0] = 0x00  # adresse du registre point
+        I2C.writeto(0x48, buf)  # écriture dans le LM75
+        mesure = I2C.readfrom(0x48, 2)  # lecture 
+        val = (mesure[0] << 3)|(mesure[1] >> 5) #reduction de 16 à 11bits.
+        if (val > 1023):                        #si valeur négative
+            res =  (val ^ 0b11111111111)+1
+            self.temperature = - res * 0.125
+        else :                                  #si valeur positive              
+            self.temperature = val * 0.125
+        print("temperature:",temperature)
+        return self.temperature
