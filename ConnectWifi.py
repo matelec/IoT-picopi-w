2fc36ad96b44f1a1de0e6f67c3bbcc8e6e6c3992
MRATTE <105413905+matelec@users.noreply.github.com>
2025-02-28 15:36:18 +0100

    Update ConnectWifi.py

--- ConnectWifi.py
+++ ConnectWifi.py
@@ -1,12 +1,33 @@
 import network
+import time
 
-def connect(ssid, key):
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(ssid, key)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
-    return sta_if
+class WiFiConnection:
+    def __init__(self, ssid, key, max_retries=10):
+        self.ssid = ssid
+        self.key = key
+        self.max_retries = max_retries
+        self.sta_if = network.WLAN(network.STA_IF)
+
+    def connect(self):
+        if not self.sta_if.isconnected():
+            print('Connecting to network...')
+            self.sta_if.active(True)
+            self.sta_if.connect(self.ssid, self.key)
+            
+            retries = 0
+            while not self.sta_if.isconnected() and retries < self.max_retries:
+                time.sleep(1)
+                retries += 1
+                print(f'Retrying... {retries}/{self.max_retries}')
+            
+            if not self.sta_if.isconnected():
+                print("Failed to connect to the network.")
+                return None
+        
+        print('Network config:', self.sta_if.ifconfig())
+        return self.sta_if
+
+    def disconnect(self):
+        if self.sta_if.isconnected():
+            self.sta_if.disconnect()
+            print('Disconnected from the network.')
