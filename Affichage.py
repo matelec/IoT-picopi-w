985bf9fb5e8788ec5502ebb197e7245a122d5e67
MRATTE <matthias.ratte@gmail.com>
2025-02-28 16:43:41 +0100

    Update Affichage.py

--- Affichage.py
+++ Affichage.py
@@ -8,7 +8,7 @@ class AffichageOled:
         self.i2c = i2c
         self.oled = ssd1306.SSD1306_I2C(width, height, i2c)
 
-    def oledTemp(self):
+    def oledTemp(self,temperature):
         self.oled.fill(0)
         self.oled.fill_rect(0, 0, 32, 32, 1)
         self.oled.fill_rect(2, 2, 28, 28, 0)
